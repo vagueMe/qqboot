43ebc183302096dc42827827491a63a6e7523582
hudi <huxiaodilw@capinfo.com.cn>
2026-02-09 16:48:53 +0800

    使用记忆模式

---
+++ src/main/java/com/hudi/qqboot/config/PersistentChatMemoryStore.java
@@ -0,0 +1,41 @@
+package com.hudi.qqboot.config;
+
+import dev.langchain4j.data.message.ChatMessage;
+import dev.langchain4j.store.memory.chat.ChatMemoryStore;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author hudi
+ * @date 05 2月 2026 17:14
+ */
+public class PersistentChatMemoryStore implements ChatMemoryStore {
+
+    Map<String, List<ChatMessage> > temp = new HashMap<>();
+    @Override
+    public List<ChatMessage> getMessages(Object memoryId) {
+        // TODO: Implement getting all messages from the persistent store by memory ID.
+        // ChatMessageDeserializer.messageFromJson(String) and
+        // ChatMessageDeserializer.messagesFromJson(String) helper methods can be used to
+        // easily deserialize chat messages from JSON.
+        return temp.computeIfAbsent((String) memoryId, k -> new ArrayList<>());
+    }
+
+    @Override
+    public void updateMessages(Object memoryId, List<ChatMessage> messages) {
+        // TODO: Implement updating all messages in the persistent store by memory ID.
+        // ChatMessageSerializer.messageToJson(ChatMessage) and
+        // ChatMessageSerializer.messagesToJson(List<ChatMessage>) helper methods can be used to
+        // easily serialize chat messages into JSON.
+        temp.put((String) memoryId, messages);
+    }
+
+    @Override
+    public void deleteMessages(Object memoryId) {
+        // TODO: Implement deleting all messages in the persistent store by memory ID.
+        temp.remove(memoryId);
+    }
+}
